2ba74c26b53765baca5948d69ebc9676c0dd5ada
彭玉荣 <pengyr11@chinaunicom.cn>
2019-06-15 17:37:56 +0800

    分享

--- questionnaire/src/main.js
+++ questionnaire/src/main.js
@@ -28,5 +28,33 @@ const router = new VueRouter({
 let vm = new Vue({
     el: '#questionnaire',
     router,
-    ...App
+    ...App,
+    created() {
+        let wxInfo = {}
+        this.$axios.get(`/questionnaire/wechat/getJsSdkInfo?url=${location.href.split('#')[0]}`).then(res => {
+            wxInfo = res.data
+        })
+        wxInfo.debug = true;
+        wxInfo.jsApiList = ['onMenuShareTimeline',"onMenuShareAppMessage"];
+        // wx.config({
+        //     debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        //     appId: 'wx78752336f9890a8d', // 必填，公众号的唯一标识
+        //     timestamp: 1560493078, // 必填，生成签名的时间戳
+        //     nonceStr: 'shty', // 必填，生成签名的随机串
+        //     signature: '69fb41cd6681bb6e123b38181a6f35d7c9ab742a',// 必填，签名
+        //     jsApiList: ['onMenuShareTimeline'] // 必填，需要使用的JS接口列表
+        // });
+        wx.config(wxInfo)
+
+        wx.ready(function () {
+            wx.onMenuShareTimeline({
+                title: '问卷分享', // 分享标题
+                link: location.href.split('#')[0], // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: 'http://www.laststory.cn/img/logo.png', // 分享图标
+                success: function () {
+                    // 设置成功
+                }
+            })
+        });
+    }
 })
