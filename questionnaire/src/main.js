db225379d5c7e69c660d8c16c2ad01cf953e6b5c
彭玉荣 <pengyr11@chinaunicom>
2019-06-18 16:50:08 +0800

    debug模式修改

--- questionnaire/src/main.js
+++ questionnaire/src/main.js
@@ -33,7 +33,7 @@ let vm = new Vue({
         let wxInfo = {}
         this.$axios.get(`/questionnaire/wechat/getJsSdkInfo?url=${encodeURIComponent(location.href.split('#')[0])}`).then(res => {
             wxInfo = res.data;
-            wxInfo.debug = true;
+            wxInfo.debug = false;
             wxInfo.jsApiList = ['onMenuShareTimeline', "onMenuShareAppMessage"];
             // wx.config({
             //     debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
