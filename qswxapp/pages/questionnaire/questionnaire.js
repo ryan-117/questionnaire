788f82ac3628a8b9af2b7a2271d2e7d8095e08b4
彭玉荣 <pengyr11@chinaunicom>
2019-06-27 20:54:36 +0800

    小程序首页

---
+++ qswxapp/pages/questionnaire/questionnaire.js
@@ -0,0 +1,37 @@
+//index.js
+//获取应用实例
+const app = getApp();
+const baseUrl = "http://49.234.19.139:9999/questionnaire/";
+
+Page({
+    data: {},
+    qsIdInput: function(e) {
+        this.setData({
+            qsId: e.detail.value
+        })
+    },
+    getQsdata: function(e) {
+        let that = this;
+        let qsId = this.data.qsId || 83;
+        wx.request({
+            url: baseUrl + `questionnaire/loadQsnaire/${qsId}`,
+            method: "GET",
+            complete: ({data}) => {
+                if(data.code === 0) {
+                    that.setData({
+                        searchResultQsData: data.data
+                    })
+                }
+            }
+        })
+    },
+    //事件处理函数
+    bindViewTap: function () {
+        wx.navigateTo({
+            url: '../logs/logs'
+        })
+    },
+    onLoad: function () {
+
+    },
+})
